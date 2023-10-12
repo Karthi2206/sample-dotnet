0e522df9dfbe32447cc2622e523ba9738ddc798c
karthigeyan92cloud <karthigeyan92cloud@gmail.com>
2023-10-12 10:41:51 +0530

    update dotnet files

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebAPIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        public void  Getmethod()
+        {
+            Console.WriteLine("It is a get method");
+        }
+    }
+}
